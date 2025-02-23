1fd6774da074525919cc0bbd5a2211261a278b10
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2025-02-23 23:44:16 +0100

    History main page done, location images and details to be added

---
+++ web_development_1_boilerplate-advanced-version/web_development_1_boilerplate-advanced-version/app/public/views/pages/history.php
@@ -0,0 +1,213 @@
+
+<head>
+    <meta charset="UTF-8">
+    <!-- Leaflet CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <!-- Leaflet JavaScript -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="../../assets/css/navbarStyle.css">
+    <link rel="stylesheet" href="../../assets/css/historyStyle.css">
+    <link rel="stylesheet" href="../../assets/css/footer.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php
+    $activePage = 'history';
+    require_once(__DIR__ . "/../partials/navbar.php");
+    ?>
+    <!-- About History Section -->
+    <div  class="about-history-section">
+        <div class="about-text">
+            <h1>A Stroll through History</h1>
+            <h2>General information</h2>
+            <p>A Stroll through History is a 2.5 hour long walking event where you discover and learn about important landmarks of Haarlems history, from churches to courts, together with 11 others.As this stroll is of an historic nature, participants have to be of ages 12 or up and no strollers are allowed during the stroll. The stroll will visit 9 historical landmarks and a has a break after the 5th location, the Jopenkerk. During the break there will be 1 free drink per participant.</p>
+            <h3>Pricing</h3>
+            <p>Tickets for a Stroll through History are sold as personal ticket or as a family ticket. <br>• Regular Participant: € 17,50  <br>• Family ticket (max. 4 participants): € 60,-</p>
+        </div>
+    </div>
+    <div class="schedule-area">
+        <div>
+            <h2>Thursday</h2>
+            <div class="schedule-item-collection">
+                <div class="schedule-item">
+                    <div class="top"><h2>10:00</h2></div>
+                    <div class="bottom">
+                        <p>1 Dutch tour<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>1 English tour<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="top"><h2>13:00</h2></div>
+                    <div class="bottom">
+                        <p>1 Dutch tour<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>1 English tour<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="top"><h2>16:00</h2></div>
+                    <div class="bottom">
+                        <p>1 Dutch tour<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>1 English tour<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h2>Friday</h2>
+            <div class="schedule-item-collection">
+                <div class="schedule-item">
+                    <div class="top"><h2>10:00</h2></div>
+                    <div class="bottom">
+                        <p>1 Dutch tour<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>1 English tour<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="top"><h2>13:00</h2></div>
+                    <div class="bottom">
+                        <p>1 Dutch tour<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>1 English tour<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <p>1 Chinese tour<img src="https://em-content.zobj.net/source/twitter/408/flag-china_1f1e8-1f1f3.png" alt="Chinese flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="top"><h2>16:00</h2></div>
+                    <div class="bottom">
+                        <p>1 Dutch tour<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>1 English tour<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h2>Saturday</h2>
+            <div class="schedule-item-collection">
+                <div class="schedule-item">
+                    <div class="top"><h2>10:00</h2></div>
+                    <div class="bottom">
+                        <p>2 Dutch tours<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>2 English tours<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="top"><h2>13:00</h2></div>
+                    <div class="bottom">
+                        <p>2 Dutch tours<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>2 English tours<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <p>1 Chinese tour<img src="https://em-content.zobj.net/source/twitter/408/flag-china_1f1e8-1f1f3.png" alt="Chinese flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="top"><h2>16:00</h2></div>
+                    <div class="bottom">
+                        <p>1 Dutch tour<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>1 English tour<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <p>1 Chinese tour<img src="https://em-content.zobj.net/source/twitter/408/flag-china_1f1e8-1f1f3.png" alt="Chinese flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h2>Sunday</h2>
+            <div class="schedule-item-collection">
+                <div class="schedule-item">
+                    <div class="top"><h2>10:00</h2></div>
+                    <div class="bottom">
+                        <p>2 Dutch tours<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>2 English tours<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <p>1 Chinese tour<img src="https://em-content.zobj.net/source/twitter/408/flag-china_1f1e8-1f1f3.png" alt="Chinese flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="top"><h2>13:00</h2></div>
+                    <div class="bottom">
+                        <p>3 Dutch tours<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>3 English tours<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <p>2 Chinese tours<img src="https://em-content.zobj.net/source/twitter/408/flag-china_1f1e8-1f1f3.png" alt="Chinese flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="top"><h2>16:00</h2></div>
+                    <div class="bottom">
+                        <p>1 Dutch tour<img src="https://em-content.zobj.net/source/twitter/408/flag-netherlands_1f1f3-1f1f1.png" alt="Dutch flag" width="20" height="15"></p>
+                        <p>1 English tour<img src="https://em-content.zobj.net/source/twitter/408/flag-united-kingdom_1f1ec-1f1e7.png" alt="UK flag" width="20" height="15"></p>
+                        <div class="ticketButton">Buy tickets</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <img class="tourMap" src="https://s3-alpha-sig.figma.com/img/c924/1021/f98b646d9aba34925d54e0bfb6fbe5e7?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FDt4YrLVxVVmZPpdfJwQrHTbKplwvZYmLquK6DA3UVZGcGdcLDq3U-udwsvsS3MxxBOIZWibYEqI1NvbV5zczzATGUIrPWJk2VhKdkWwDKWCxSlajN9Znw5YWmI6XC3Ok584ZrwUV55nlDhedp-Wx3fQoFVB0xkzyZR5LDW22oySFx8HNpM2DBs~0IHpz3VCuHuUZFcu1~399grhdYZ1~kHa0nVFusLQwKnol7I63ZHOg0AywQkhuIWLvVTXlY2QHhXhbR9sv-HnI9Lv6Ig835lNrtquiuMie8PafvmGvTJyDT49E0dmVGekcM~QSfPzwGTUiu6zV2hXZtyEXwovVQ__" alt="Tour map" width="941" height="554">
+    <div class="locationContainer">
+        <div class="locationCard left">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard right">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard left">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard right">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard left">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard right">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard left">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard right">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard left">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+        <div class="locationCard right">
+            <img src="" width="200" height="150" alt="Location Image">
+            <div class="locationButton">Learn more</div>
+            <p class="locationCardText">Example text for a location</p>
+        </div>
+    </div>
+    <div class="none">
+        <?php
+            require(__DIR__ . "/../partials/footer.php");
+        ?>
+    </div>
+
+    <script src="../../assets/js/maps.js"></script>
+</body>
+</html>
