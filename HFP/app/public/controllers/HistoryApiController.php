8e12c13e1393e125260becb2fa4882bc9840d266
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2025-04-07 23:32:53 +0200

    Added full history api CRUD and confirmed they work

--- HFP/app/public/controllers/HistoryApiController.php
+++ HFP/app/public/controllers/HistoryApiController.php
@@ -2,6 +2,7 @@
 
 require_once(__DIR__ . '/../models/HistoryModel.php');
 require_once(__DIR__ . '/../api/utils/ResponseHelper.php');
+require_once(__DIR__ . '/../middleware/apiAuthMiddleware.php'); 
 
 class HistoryAPIController{
     private $historyModel;
@@ -35,6 +36,82 @@ class HistoryAPIController{
             ResponseHelper::sendJson($schedule);
         } else {
             ResponseHelper::sendError('Schedule not found', 404);
+        }
+    }
+    public function createScheduleItem() {
+        //requireApiRole(['admin']);
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (isset($data['language']) && isset($data['maxTickets']) && isset($data['timeStamp'])) {
+            $scheduleItem = $this->historyModel->createScheduleItem($data);
+            ResponseHelper::sendJson($scheduleItem);
+        }
+        else {
+            ResponseHelper::sendError('Missing information for item creation', 404);
+        }
+    }
+    public function deleteScheduleItem($id) {
+        //requireApiRole(['admin']);
+        $success = $this->historyModel->deleteScheduleItem($id);
+        if ($success)
+        {
+            ResponseHelper::sendJson('Successfully deleted item with id: ' . $id);
+        }
+        else {
+            ResponseHelper::sendError('Could not delete item with id: ' . $id, 404);
+        }
+    }
+    public function updateScheduleItem($id) {
+        //requireApiRole(['admin']);
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (isset($data['language']) && isset($data['maxTickets']) && isset($data['timeStamp']) && isset($id)) {
+            $scheduleItem = $this->historyModel->updateScheduleItem($id, $data);
+            ResponseHelper::sendJson($scheduleItem);
+        }
+        else {
+            ResponseHelper::sendError('Missing information for item updates', 404);
+        }
+    }
+    public function getScheduleItemById($id) {
+        $scheduleItem = $this->historyModel->getScheduleItemById($id);
+        if ($scheduleItem) {
+            ResponseHelper::sendJson($scheduleItem);
+        } else {
+            ResponseHelper::sendError('Schedule item not found', 404);
+        }
+    }
+    public function createLocation() {
+        //requireApiRole(['admin']);
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (isset($data['image_name']) && isset($data['name']) && isset($data['description'])) {
+            $location = $this->historyModel->createLocation($data);
+            ResponseHelper::sendJson($location);
+        }
+        else {
+            ResponseHelper::sendError('Missing information for location creation', 404);
+        }
+    }
+    public function deleteLocation($id) {
+        //requireApiRole(['admin']);
+        $success = $this->historyModel->deleteLocation($id);
+        if ($success)
+        {
+            ResponseHelper::sendJson('Successfully deleted location with id: ' . $id);
+        }
+        else {
+            ResponseHelper::sendError('Could not delete location with id: ' . $id, 404);
+        }
+    }
+    public function updateLocation($id) {
+        //requireApiRole(['admin']);
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (isset($data['image_name']) && isset($data['name']) && isset($data['description']) && isset($id)) {
+            $location = $this->historyModel->updateLocation($id, $data);
+            ResponseHelper::sendJson($location);
+        }
+        else {
+            ResponseHelper::sendError('Missing information for location updates', 404);
         }
     }
 }
--- HFP/app/public/routes/api/HistoryApiRoutes.php
+++ HFP/app/public/routes/api/HistoryApiRoutes.php
@@ -2,13 +2,37 @@
 require_once(__DIR__ . '/../../controllers/HistoryAPIController.php');
 $controller = new HistoryAPIController();
 
+//Schedule api routes
 Route::add('/api/history/schedule', function () use ($controller) {
     $controller->getFullSchedule();
 }, ['GET']);
+Route::add('/api/history/schedule', function() use ($controller) {
+    $controller->createScheduleItem();
+}, ['POST']);
+Route::add('/api/history/schedule/([0-9]*)', function($id) use ($controller) {
+    $controller->deleteScheduleItem($id);
+}, ['DELETE']);
+Route::add('/api/history/schedule/([0-9]*)', function($id) use ($controller) {
+    $controller->updateScheduleItem($id);
+}, ['PUT']);
+Route::add('/api/history/schedule/([0-9]*)', function($id) use ($controller) {
+    $controller->getScheduleItemById($id);
+}, ['GET']);
+
+//Locations api routes
 Route::add('/api/history/locations', function() use ($controller) {
     $controller->getAllLocations();
 }, ['GET']);
-Route::add('/api/history/locations/([a-z-0-9-]*)', function ($id) use ($controller) {
+Route::add('/api/history/locations/([0-9]*)', function ($id) use ($controller) {
     $controller->getLocationById($id);
 }, ['GET']);
+Route::add('/api/history/locations', function () use ($controller) {
+    $controller->createLocation();
+}, ['POST']);
+Route::add('/api/history/locations/([0-9]*)', function ($id) use ($controller) {
+    $controller->deleteLocation($id);
+}, ['DELETE']);
+Route::add('/api/history/locations/([0-9]*)', function ($id) use ($controller) {
+    $controller->updateLocation($id);
+}, ['PUT']);
 ?>
